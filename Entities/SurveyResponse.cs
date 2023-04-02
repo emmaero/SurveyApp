acf914fef0915e33dbc903d7469d92f33bab9362
Emmanuel Imhontu <imhontu.emmanuel@yahoo.com>
2023-04-02 18:06:45 +0200

    Database table validation

--- Entities/SurveyResponse.cs
+++ Entities/SurveyResponse.cs
@@ -5,13 +5,15 @@ namespace SurveyTest.Entities
 {
     public class SurveyResponse
     {
+        [Required]
         public int Id { get; set; }
-   
+        [Required]
         public string Name { get; set; }
-
+        [Required]
         public int Age { get; set; }
-
+        [Required]
         public string Gender { get; set; }
+        [Required]
         public string Question { get; set; }
 
     
